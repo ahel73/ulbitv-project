a542493e330b9c548f563db33e682f39e42c20d7
Нагаев Алексей <nag495@yandex.ru>
2022-09-21 21:56:04 +0300

    1-7 разобради как добавлять стили подключать тему т сделали хук для смены тем

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useContext } from "react";
 import { Link, Route, Routes } from 'react-router-dom';
-import './index.scss';
+import './style/index.scss';
 
 import AboutPage from "./pages/AboutPage/AboutPage";
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import MainPage from './pages/MainPage/MainPage';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { ThemeContext, Theme } from './theme/ThemeContent';
+import { useTheme } from './theme/useTheme';
 
-const App = () => { 
+
+
+const App = () => {
+
+    const {theme, toggleTheme} = useTheme()
+    
     return (
-        <div className='app'>
-            <Link to={'/'}>Главная</Link>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>toggle</button>
+            <Link to={'/'}> Главная</Link>
             <Link to={'./about'}>о Сайте</Link>
             <Suspense fallback={<div>Loading....</div>}>
                 <Routes>                
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import { render } from "react-dom";
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
+import ThemeProvider from "./theme/ThemeProvider";
 
 render(
 
     <BrowserRouter>
-        <App />
-    </BrowserRouter>
-    ,
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>       
+    </BrowserRouter>,
     document.getElementById('root')
 )
